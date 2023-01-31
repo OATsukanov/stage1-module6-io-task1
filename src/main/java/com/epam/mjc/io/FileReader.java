1ae8c3277252407659be9eb218899628ccb2f01d
oleg <tsukanov.regula@gmail.com>
2023-02-01 00:17:43 +0500

    first try

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,43 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+
+        FileInputStream imputStream = null;
+        //file = new File("Profile.txt");
+
+        String profileString = "";
+
+        try {
+            imputStream = new FileInputStream(file);
+            int c;
+            while ((c = imputStream.read()) != -1) {
+                profileString += (char)c;
+            }
+        } finally {
+
+            if (imputStream != null) {
+                imputStream.close();
+            }
+        }
+
+        String[] pairs = profileString.split("\\n");
+        for (int i = 0; i < pairs.length; i++) {
+
+            pairs[i] = pairs[i].split(": ")[1];
+        }
+
+        String name = pairs[0];
+        int age = Integer.parseInt(pairs[1]);
+        String email = pairs[2];
+        long phone = Long.parseLong(pairs[3]);
+
+        return new Profile(name, age, email, phone);
     }
 }
